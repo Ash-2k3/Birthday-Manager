35320d4aff8efbb545c311af16bdd20efd6fb5d0
Ash-2k3 <ash.me.cse@gmail.com>
2023-06-29 13:17:30 +0530

    New script

---
+++ birthday_wisher.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import datetime
+import smtplib
+from email.message import EmailMessage
+import os
+
+# Interaction with Email File
+
+if __name__ == "__main__":
+           df = pd.read
